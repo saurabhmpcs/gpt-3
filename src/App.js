628877f7c7f4fae930e9add95b06fbd7f5b071f3
Saurabh <saurabh.rgt@gmail.com>
2023-03-08 12:35:51 +0530

    8_march

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ import "./App.css";
 const App = () => {
   return (
     <div className="App">
-      <div className="gradient-bg">
+      <div className="gradient__bg">
         <Navbar />
         <Header />
       </div>
